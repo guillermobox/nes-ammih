f9bf7945a1d186850e6b1bcc5b77393f73a8731d
Guillermo Indalecio <guillermobox@gmail.com>
2020-04-13 23:49:23 +0200

    Small tweak in computer

--- initialize.s
+++ initialize.s
@@ -93,7 +93,7 @@ initializePalette:
 defaultPalette:
 	; the background values for sprites
 	; have priority because of mirroring
-	.byte $3f,$30,$27,$00 ; background 0
+	.byte $3f,$10,$20,$00 ; background 0
 	.byte $3f,$3f,$3f,$3f ; background 1
 	.byte $3f,$3f,$3f,$3f ; background 2
 	.byte $3f,$3f,$3f,$3f ; background 3
